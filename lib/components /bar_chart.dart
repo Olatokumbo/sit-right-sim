0c353dd94db09e0718eaba2d32c9c91fd2df2b31
Olatokumbo <faithodesola@gmail.com>
2024-07-10 06:28:48 +0100

    Add posture legend

--- lib/components /bar_chart.dart
+++ lib/components /bar_chart.dart
@@ -6,89 +6,124 @@ import 'package:sit_right_app/utils.dart';
 class BarChartWidget extends StatelessWidget {
   final List<PostureStatistics> statistics;
 
-  const BarChartWidget(this.statistics, {Key? key}) : super(key: key);
+  const BarChartWidget(this.statistics, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Calculate maximum duration for scaling
     double maxY = _getMaxY(statistics);
 
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: statistics.isEmpty
-            ? const Center(
-                child: Text(
-                  'No data available',
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              )
-            : BarChart(
-                BarChartData(
-                  alignment: BarChartAlignment.spaceAround,
-                  maxY: maxY,
-                  barTouchData: BarTouchData(enabled: false),
-                  titlesData: FlTitlesData(
-                    leftTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
+    return Column(children: [
+      Expanded(
+        flex: 1,
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: statistics.isEmpty
+                ? const Center(
+                    child: Text(
+                      'No data available',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
                     ),
-                    topTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                    bottomTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: false,
-                        getTitlesWidget: (double value, TitleMeta meta) {
-                          int index = value.toInt();
-                          if (index < 0 || index >= statistics.length) {
-                            return const SizedBox.shrink();
-                          }
-                          return Padding(
-                            padding: const EdgeInsets.only(top: 1.0),
-                            child: Text(statistics[index].posture),
-                          );
-                        },
+                  )
+                : BarChart(
+                    BarChartData(
+                      alignment: BarChartAlignment.spaceAround,
+                      maxY: maxY,
+                      minY: -0.5,
+                      barTouchData: BarTouchData(enabled: false),
+                      titlesData: FlTitlesData(
+                        leftTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        topTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: (double value, TitleMeta meta) {
+                              DateTime startTime = DateTime.now()
+                                  .subtract(const Duration(hours: 2));
+                              DateTime time = startTime
+                                  .add(Duration(minutes: value.toInt()));
+                              return Padding(
+                                padding: const EdgeInsets.only(top: 1.0),
+                                child: Text(
+                                  _formatTime(time),
+                                  style: const TextStyle(fontSize: 10),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        rightTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            reservedSize: 40,
+                            getTitlesWidget: (double value, TitleMeta meta) {
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 4.0),
+                                child: Text(
+                                  (value / 1000000).toStringAsFixed(0),
+                                  style: const TextStyle(fontSize: 10),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                       ),
-                    ),
-                    rightTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 40,
-                        getTitlesWidget: (double value, TitleMeta meta) {
-                          return Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 4.0),
-                            child: Text(
-                              value.toStringAsFixed(0),
-                              style: const TextStyle(fontSize: 10),
-                            ),
-                          );
-                        },
-                      ),
+                      borderData: FlBorderData(show: false),
+                      barGroups: _createBarGroups(statistics),
                     ),
                   ),
-                  borderData: FlBorderData(show: false),
-                  barGroups: _createBarGroups(statistics),
-                ),
-              ),
+          ),
+        ),
       ),
-    );
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          "Upright",
+          "Leaning Back",
+          "Leaning Left",
+          "Leaning Right",
+          "Slouching"
+        ].map((posture) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Row(
+              children: [
+                Container(
+                    width: 20, height: 20, color: getColorByPosture(posture)),
+                const SizedBox(
+                    width: 10), // Add some space between the box and the text
+                Text(posture),
+              ],
+            ),
+          );
+        }).toList(),
+      )
+    ]);
   }
 
   List<BarChartGroupData> _createBarGroups(List<PostureStatistics> statistics) {
-    return statistics.asMap().entries.map((entry) {
-      int index = entry.key;
-      PostureStatistics stat = entry.value;
+    DateTime startTime = DateTime.now().subtract(const Duration(hours: 2));
+
+    return statistics.map((stat) {
+      int x = stat.startTime.difference(startTime).inMinutes;
       Color barColor = getColorByPosture(stat.posture);
 
       return BarChartGroupData(
-        x: index,
+        x: x,
         barRods: [
           BarChartRodData(
-            toY: stat.endTime.difference(stat.startTime).inSeconds.toDouble(),
+            toY: stat.endTime
+                .difference(stat.startTime)
+                .inMicroseconds
+                .toDouble(),
             color: barColor,
             width: 16,
           ),
@@ -104,10 +139,10 @@ class BarChartWidget extends StatelessWidget {
     double maxTime = statistics
         .map(
           (stat) =>
-              stat.endTime.difference(stat.startTime).inSeconds.toDouble(),
+              stat.endTime.difference(stat.startTime).inMicroseconds.toDouble(),
         )
         .reduce((a, b) => a > b ? a : b);
-    return maxTime * 1.1; // Adding some space on top of the highest bar
+    return maxTime * 1.1;
   }
 
   String _formatTime(DateTime time) {
